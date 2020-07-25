db9e076e656b295822fb6659aa3dbea5d1751a79
Aryan <aryan.r.t@hotmail.com>
2020-07-25 14:55:21 -0400

    ScroogeCoin

--- ScroogCoin/TxHandler.java
+++ ScroogCoin/TxHandler.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class TxHandler {
 
     /**
@@ -5,7 +7,10 @@ public class TxHandler {
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
      * constructor.
      */
+    private UTXOPool utxoPool;
     public TxHandler(UTXOPool utxoPool) {
+
+        this.utxoPool = new UTXOPool(utxoPool);
         // IMPLEMENT THIS
     }
 
@@ -19,6 +24,48 @@ public class TxHandler {
      *     values; and false otherwise.
      */
     public boolean isValidTx(Transaction tx) {
+    	
+        byte[] hash = tx.getHash();
+        double outputTotal=0, inputTotal=0;
+
+        //(1)
+        for(int i =0; i < tx.numInputs(); i++)
+            if(! utxoPool.contains(new UTXO(hash,i)))
+                return false;
+
+        //(2)
+        for(int i =0; i < tx.numInputs(); i++)
+        {
+        	UTXO temp = new UTXO(tx.getInput(i).prevTxHash, tx.getInput(i).outputIndex);
+        	
+            if(! Crypto.verifySignature(utxoPool.getTxOutput(temp).address ,temp.getTxHash(), tx.getInput(i).signature))
+                return false;
+        }
+        
+        //(3)
+        for(UTXO u: utxoPool.getAllUTXO())
+        	if(u.getTxHash().equals(tx.getHash()))
+        		return false;
+        //(4)
+        for(int i =0; i < tx.numOutputs(); i++)
+        {
+        	double temp = tx.getOutput(i).value;
+        	if(temp < 0 )
+        		return false;
+        	outputTotal += temp;
+        }
+        //(5)
+        for(int i =0; i < tx.numOutputs(); i++)
+        	inputTotal += tx.getOutput(i).value;
+        
+        if(outputTotal > inputTotal)
+        	return false;
+        
+        
+        return true;
+
+
+        
         // IMPLEMENT THIS
     }
 
@@ -28,6 +75,21 @@ public class TxHandler {
      * updating the current UTXO pool as appropriate.
      */
     public Transaction[] handleTxs(Transaction[] possibleTxs) {
+    	ArrayList<Transaction> result = new ArrayList<Transaction>();
+    	
+    	for(Transaction tx : possibleTxs)
+    	{
+    		if(this.isValidTx(tx))
+    		{
+    			result.add(tx);
+    	        for(int i =0; i < tx.numOutputs(); i++)
+    	        {
+    	        	UTXO temp = new UTXO(tx.getHash(), i);
+    	        	utxoPool.addUTXO(temp, tx.getOutput(i));
+    	        }
+    		}
+    	}
+    	return (Transaction[])result.toArray();
         // IMPLEMENT THIS
     }
 
